3c5330fc9dfa9a47aeeb0988ef00e3d8df643e58
Su Yang <soulteary@users.noreply.github.com>
2024-01-02 21:31:33 +0800

    feat: update gin-static embed demo

--- example/embed/main.go
+++ example/embed/main.go
@@ -9,19 +9,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed data
-var server embed.FS
+//go:embed public
+var EmbedFS embed.FS
 
 func main() {
 	r := gin.Default()
-	r.Use(static.Serve("/", static.EmbedFolder(server, "data/server")))
+
+	r.Use(static.Serve("/", static.EmbedFolder(EmbedFS, "public/page")))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "test")
 	})
+
 	r.NoRoute(func(c *gin.Context) {
 		fmt.Printf("%s doesn't exists, redirect on /\n", c.Request.URL.Path)
 		c.Redirect(http.StatusMovedPermanently, "/")
 	})
+
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
