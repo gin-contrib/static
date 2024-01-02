7cfca13d5ec7a6687a6ca3cadd593d239b3c3b75
Su Yang <soulteary@users.noreply.github.com>
2024-01-02 21:27:30 +0800

    feat: update gin-static demo

--- example/simple/main.go
+++ example/simple/main.go
@@ -11,14 +11,16 @@ func main() {
 	r := gin.Default()
 
 	// if Allow DirectoryIndex
-	// r.Use(static.Serve("/", static.LocalFile("/tmp", true)))
+	// r.Use(static.Serve("/", static.LocalFile("./public", true)))
 	// set prefix
-	// r.Use(static.Serve("/static", static.LocalFile("/tmp", true)))
+	// r.Use(static.Serve("/static", static.LocalFile("./public", true)))
 
-	r.Use(static.Serve("/", static.LocalFile("/tmp", false)))
+	r.Use(static.Serve("/", static.LocalFile("./public", false)))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "test")
 	})
+
 	// Listen and Server in 0.0.0.0:8080
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
