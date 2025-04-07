3e60afc60589fa7d7f6be3f4b1f1d3f06aad55e9
Bo-Yi Wu <appleboy.tw@gmail.com>
2025-04-07 15:20:34 +0800

    docs: improve error handling and documentation for EmbedFolder function
    - Add error handling for the `EmbedFolder` function
    - Add comments explaining the `embedFileSystem` struct and its `Exists` method
    - Add imports for `errors` and `strings` packages
    - Update `TestEmbedFolder` to handle errors from `EmbedFolder` function
    - Add comments in `embed_folder_test.go` in Chinese for test case explanations
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- embed_folder.go
+++ embed_folder.go
@@ -2,30 +2,41 @@ package static
 
 import (
 	"embed"
+	"errors"
 	"io/fs"
 	"log/slog"
 	"net/http"
+	"strings"
 )
 
+// embedFileSystem is a struct that implements the http.FileSystem interface for embedding a file system.
 type embedFileSystem struct {
 	http.FileSystem
 }
 
+// Exists method checks if the given file path exists in the embedded file system.
+// If the path exists, it returns true; otherwise, it returns false.
 func (e embedFileSystem) Exists(prefix string, path string) bool {
+	if len(prefix) > 1 && strings.HasPrefix(path, prefix) {
+		path = strings.TrimPrefix(path, prefix)
+	}
+
 	_, err := e.Open(path)
 	return err == nil
 }
 
-func EmbedFolder(fsEmbed embed.FS, targetPath string) ServeFileSystem {
+// EmbedFolder function embeds the target folder from the embedded file system into the ServeFileSystem.
+// If an error occurs during the embedding process, it returns the error message.
+func EmbedFolder(fsEmbed embed.FS, targetPath string) (ServeFileSystem, error) {
 	fsys, err := fs.Sub(fsEmbed, targetPath)
 	if err != nil {
 		slog.Error("Failed to embed folder",
 			"targetPath", targetPath,
 			"error", err,
 		)
-		return nil
+		return nil, errors.New("failed to embed folder: " + err.Error())
 	}
 	return embedFileSystem{
 		FileSystem: http.FS(fsys),
-	}
+	}, nil
 }
